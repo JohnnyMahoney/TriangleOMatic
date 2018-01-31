6dace3c6a084bbea7265da894b4b37f6cb5f5c60
Christoph Hermanns <christoph.jan.alexander.hermanns@rwth-aachen.de>
2018-01-31 01:33:45 +0100

    Bald_fertig xD

---
+++ Triangle 'o matic/Triangle 'o matic/Dreieck.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+
+namespace Triangle__o_matic
+{
+    class Dreieck
+    {
+
+        private Point _pt1;
+        private Point _pt2;
+        private Point _pt3;
+        private int _value;
+        public Point Pt1 { get { return _pt1; } set { _pt1 = value; } }
+        public Point Pt2 { get { return _pt2; } set { _pt2 = value; } }
+        public Point Pt3 { get { return _pt3; } set { _pt3 = value; } }
+
+
+        public void Convert(int factorx, int factory)
+        {
+            _pt1.X *= factorx;
+            _pt1.Y *= factory;
+            _pt2.X *= factorx;
+            _pt2.Y *= factory;
+            _pt3.X *= factorx;
+            _pt3.Y *= factory;
+        }
+        public Point GetMax()
+        {
+            double x, y;
+            if (this.Pt1.X >= this.Pt2.X)
+            {
+                x = this.Pt1.X;
+            }
+            else
+            {
+                x = this.Pt2.X;
+            }
+            if (this.Pt1.Y >= this.Pt2.Y)
+            {
+                y = this.Pt1.Y;
+            }
+            else
+            {
+                y = this.Pt2.Y;
+            }
+
+            return new Point(x, y);
+        }
+        /*private void Validate()
+        {
+
+        }*/
+    }
+}
--- Triangle 'o matic/Triangle 'o matic/MainWindow.xaml.cs
+++ Triangle 'o matic/Triangle 'o matic/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -10,21 +11,25 @@ namespace Triangle__o_matic
     /// </summary>
     public partial class MainWindow : Window
     {
+        int leX = 20;
+        int leY = 20;
+        List<Dreieck> DreieckListe = new List<Dreieck>();
         public MainWindow()
         {
+
             InitializeComponent();
-            Draw_CoordSys();
-            Draw_Triangle();
+            Draw_CoordSys(leX, leY);
 
-
+            Make_Triangle(new Point(5, 5), "ld");
+            Make_Triangle(new Point(31, 5), "ld");
 
         }
 
-        private void Draw_CoordSys()
+        private void Draw_CoordSys(int lex, int ley)
         {
-            for (int j = 0; j <= CoordSys.Height; j += 10)
+            for (int j = 0; j <= CoordSys.Height; j += ley)
             {
-                for (int i = 0; i <= CoordSys.Width; i += 10)
+                for (int i = 0; i <= CoordSys.Width; i += lex)
                 {
                     Draw_Point(i, j);
                 }
@@ -33,23 +38,73 @@ namespace Triangle__o_matic
         }
         private void Draw_Point(int x, int y)
         {
-            Ellipse coordDot = new Ellipse() { Width = 2, Height = 2, Stroke = Brushes.Black, StrokeThickness = 2 };
-
+            Ellipse coordDot = new Ellipse() { Width = 1, Height = 1, Stroke = Brushes.Black, StrokeThickness = 1 };
+            coordDot.HorizontalAlignment = HorizontalAlignment.Center;
+            coordDot.VerticalAlignment = VerticalAlignment.Center;
             CoordSys.Children.Add(coordDot);
-
             coordDot.SetValue(Canvas.LeftProperty, (double)x);
             coordDot.SetValue(Canvas.TopProperty, (double)y);
         }
-        private void Draw_Triangle()
+        private void Draw_Triangle(Dreieck dreieck)
         {
-            Line side1 = new Line() { Width = 2, Height = 2, Stroke = Brushes.Red, StrokeThickness = 2 };
-            side1.X1 = 5;
-            side1.Y1 = 2;
-            side1.X2 = 15;
-            side1.Y2 = 2;
-            CoordSys.Children.Add(side1);
-            side1.SetValue(Canvas.LeftProperty, (double)0);
-            side1.SetValue(Canvas.TopProperty, (double)0);
+            Polygon triangle = new Polygon();
+            var points = new PointCollection();
+
+            triangle.Stroke = Brushes.Black;
+            triangle.StrokeThickness = 1;
+            triangle.Fill = Brushes.OrangeRed;
+
+            points.Add(dreieck.Pt1);
+            points.Add(dreieck.Pt2);
+            points.Add(dreieck.Pt3);
+
+            triangle.Points = points;
+            CoordSys.Children.Add(triangle);
+            DreieckListe.Add(dreieck);
         }
+        private void Make_Triangle(Point pt1, string direction)
+        {
+            Dreieck temp = new Dreieck();
+
+            temp.Pt1 = pt1;
+
+            switch (direction)
+            {
+
+
+                case "rd":
+                    temp.Pt2 = new Point(pt1.X + 1, pt1.Y);
+                    temp.Pt3 = new Point(pt1.X + 1, pt1.Y + 1);
+                    break;
+                case "ru":
+                    temp.Pt2 = new Point(pt1.X + 1, pt1.Y);
+                    temp.Pt3 = new Point(pt1.X + 1, pt1.Y - 1);
+                    break;
+                case "ld":
+                    temp.Pt2 = new Point(pt1.X - 1, pt1.Y);
+                    temp.Pt3 = new Point(pt1.X - 1, pt1.Y + 1);
+                    break;
+                case "lu":
+                    temp.Pt2 = new Point(pt1.X - 1, pt1.Y);
+                    temp.Pt3 = new Point(pt1.X - 1, pt1.Y - 1);
+                    break;
+                default:
+                    break;
+            }
+
+            temp.Convert(leX, leY);
+
+            if (temp.GetMax().X >= CoordSys.Width | temp.GetMax().Y >= CoordSys.Height)
+            {
+                temp = null;
+            }
+            else
+            {
+                Draw_Triangle(temp);
+            }
+
+        }
+
+
     }
 }
